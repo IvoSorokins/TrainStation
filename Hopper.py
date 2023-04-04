0b2c066a434af535d641576b5c781b775ede71aa
Ivo Sorokins <91875727+IvoSorokins@users.noreply.github.com>
2023-04-04 15:02:44 +0300

    Added Hopper.py

---
+++ Hopper.py
@@ -0,0 +1,30 @@
+import Carriage
+
+
+class Hopper(Carriage):
+    valid_cargo_types = ["Grain", "Sand", "Fertilizer"]
+    hp_counter = 0
+
+    def __init__(self, tare, length, volume, cargo_type="Grain"):
+        super().__init__(tare, length, volume)
+        Hopper.hp_counter += 1
+        self.hp = f"HP-{Hopper.hp_counter}"
+        self.cargo_type = cargo_type
+        self.cargo_weight = 0
+
+    def set_cargo_type(self, cargo_type):
+        if self.cargo_type in Hopper.valid_cargo_types:
+            self.cargo_type = cargo_type
+        else:
+            print("\nInvalid cargo type! \nUsing default cargo type - Grain!")
+
+    def add_cargo(self, cargo_type, cargo_weight):
+        if cargo_type in Hopper.valid_cargo_types:
+            self.cargo_type = cargo_type
+            self.cargo_weight = cargo_weight
+        else:
+            print("\nInvalid cargo type! \nUsing default cargo type - Grain!")
+
+        # Method that returns string that represents object
+    def __repr__(self):
+        return f"[Cistern] ID:{self.hp} Cargo type: {self.cargo_type} Volume:{self.volume}"
