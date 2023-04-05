bfa941c72ebf696d0cfc7e09c4d49429f10d5090
Ivo Sorokins <91875727+IvoSorokins@users.noreply.github.com>
2023-04-05 09:34:35 +0300

    Added add_last and add_first methods

--- TrainLinkedList.py
+++ TrainLinkedList.py
@@ -3,7 +3,7 @@ import Carriage
 
 class TrainLinkedList(Carriage):
     def __init__(self):
-        number = 0   # Instead of TrainLinkedList I will use number - as counters name
+        self.number = 0   # Instead of TrainLinkedList I will use number - as counters name
         self.head = None  # Used for linked list, starts from head,head pointer to keep track of the first node
         self.tail = None  # Used for linked list, points to the last node in the list
 
@@ -13,3 +13,21 @@ class TrainLinkedList(Carriage):
             self.next = None  # Linked lists must set pointers for next element in list using "next" keyword
             TrainLinkedList.number += 1
             self.number = f"TrainLinkedList-{TrainLinkedList.number - 1}"  # -1 makes it start from 0
+
+        def add_first(self, carriage):
+            new_node = TrainLinkedList.Node(carriage)
+            if self.head is None:
+                self.head = new_node
+                self.tail = new_node
+            else:
+                new_node.next = self.head
+                self.head = new_node
+
+        def add_last(self, carriage):
+            new_node = TrainLinkedList.Node(carriage)
+            if self.tail is None:
+                self.head = new_node
+                self.tail = new_node
+            else:
+                self.tail.next = new_node
+                self.tail = new_node
