761e4cb996d539a8386ec0935e15f6df0db8d2b0
Ivo Sorokins <91875727+IvoSorokins@users.noreply.github.com>
2023-04-05 09:21:27 +0300

    Updated TrainLinkedList to be Linked List Class

--- TrainLinkedList.py
+++ TrainLinkedList.py
@@ -2,12 +2,14 @@ import Carriage
 
 
 class TrainLinkedList(Carriage):
-    number = 0
+    def __init__(self):
+        number = 0   # Instead of TrainLinkedList I will use number - as counters name
+        self.head = None  # Used for linked list, starts from head,head pointer to keep track of the first node
+        self.tail = None  # Used for linked list, points to the last node in the list
 
     class Node:
         def __init__(self, carriage):
             self.carriage = carriage
-            self.next = None
+            self.next = None  # Linked lists must set pointers for next element in list using "next" keyword
             TrainLinkedList.number += 1
-            self.id = f"TrainLinkedList-{TrainLinkedList.number - 1}"  # -1 makes it start from 0
-
+            self.number = f"TrainLinkedList-{TrainLinkedList.number - 1}"  # -1 makes it start from 0
