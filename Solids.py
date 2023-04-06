de8de8c491ca0510432e3cd8ee6dc0bef3064f86
Ivo Sorokins <91875727+IvoSorokins@users.noreply.github.com>
2023-04-06 09:09:52 +0300

    Added classmethod

--- Solids.py
+++ Solids.py
@@ -8,3 +8,7 @@ class Solids(Enum):
     WOOD = "Wood"
     METAL_PIPES = "Metal pipes"
     SHIPPING_CONTAINER = "Shipping container"
+
+    @classmethod  # Class method that returns a list of all the values in the enumeration class
+    def as_list(cls):  # cls refers to the enumeration class itself
+        return [c.value for c in cls]  # iterates over  all the instances of enum class
