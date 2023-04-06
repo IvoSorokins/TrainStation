20a3d4bc4092f5b18d54e7d4f24cea1f0786705c
Ivo Sorokins <91875727+IvoSorokins@users.noreply.github.com>
2023-04-06 11:54:29 +0300

    Updated

--- main.py
+++ main.py
@@ -6,57 +6,69 @@ from TrainLinkedList import TrainLinkedList
 from TrainManager import TrainManager
 
 
+train = TrainLinkedList()
 
-
-
-
-train = TrainLinkedList.TrainLinkedList()
-
+Border = ('\033[33m'+"================================================================================================="
+                     "========" + '\033[0m')  # Just for visuals
 
 # First train
+
 box_cart = BoxCar(tare=10, length=200)
-# cistern = Cistern(tare=10, length=250)
-# gondola = Gondola(tare=15, length=350)
-# hopper = Hopper(tare=20, length=120)
-# box_cart.add_cargo('cattle', 20)
-# cistern.add_cargo('oil', 15)
-# hopper.add_cargo('sand', 20)
-# gondola.add_cargo('coal', 20)
-#
-# # Carriage print out
-# print(box_cart)
-# print(cistern)
-# print(hopper)
-# print(gondola)
-#
-#
-# train.add_first(box_cart)
-# train.add_last(cistern)
-# print(train)
-# train.add_after(box_cart, gondola)
-# train.add_before(gondola, hopper)
-# train.remove_carriage(box_cart)
-# print(train.get_carriage_count())
-#
-#
-# # Second train
-# second_train = TrainLinkedList()
-# box_cart1 = BoxCar(50, 200)
-# box_cart1.add_cargo('food', 30)
-# cistern1 = Cistern(10, 250)
-# gondola1 = Gondola(15, 350)
-# cistern1.add_cargo('oil', 30)
-# gondola1.add_cargo('coal', 21)
-#
-# second_train.add_first(box_cart1)
-# second_train.add_last(gondola1)
-# second_train.add_after(box_cart1, cistern1)
-#
-# train_manager = TrainManager()
-# train_manager.add_train(train)
-# train_manager.add_train(second_train)
-# print(train_manager.avg_mean_carriages())
-# print(train_manager.std_carriage())
+cistern = Cistern(tare=10, length=250)
+gondola = Gondola(tare=15, length=350)
+hopper = Hopper(tare=20, length=120)
+
+# Adding cargo to Carriages
+box_cart.add_cargo('Cattle', 20)
+cistern.add_cargo('Oil', 15)
+hopper.add_cargo('Sand', 20)
+gondola.add_cargo('Coal', 20)
+
+# Carriage print out
+print(Border+"\nFirst Train Carriages:\n")
+
+print(box_cart)
+print(cistern)
+print(hopper)
+print(gondola)
+
+print(Border)
+
+
+train.add_first(box_cart)
+train.add_last(cistern)
+print("First Train:\n")
+print(train)
+print(Border)
+
+#                New       After
+train.add_after(gondola, box_cart)
+train.add_before(hopper, gondola)
+train.remove_carriage(box_cart)
+print("First Train carriage count after adding after and add before method is called: "+str(train.get_carriage_count()))
+# Output should be 3
+print(Border)
+
+
+# Second train
+second_train = TrainLinkedList()
+box_cart1 = BoxCar(50, 200)
+box_cart1.add_cargo('Food', 30)
+cistern1 = Cistern(10, 250)
+gondola1 = Gondola(15, 350)
+cistern1.add_cargo('Oil', 30)
+gondola1.add_cargo('Coal', 21)
+
+second_train.add_first(box_cart1)
+second_train.add_last(gondola1)
+#                       New        After
+second_train.add_after(cistern1, box_cart1)
+
+train_manager = TrainManager()
+train_manager.add_train(train)
+train_manager.add_train(second_train)
+print(train_manager.avg_mean_carriages())
+print(train_manager.std_carriage())
 #
 #
 # train_manager.export_as_json('temp.json')
