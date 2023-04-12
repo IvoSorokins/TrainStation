a127b198a2cfe0cd0b09187e8b8c6e5a55068055
Ivo Sorokins <91875727+IvoSorokins@users.noreply.github.com>
2023-04-12 20:32:36 +0300

    Added docstring, more descriptive variable.

--- Carriage.py
+++ Carriage.py
@@ -1,11 +1,25 @@
 class Carriage:
     def __init__(self, tare, length, volume=0):
+        """
+        Initialize Carriage object with tare, length and volume.
+
+        Args:
+            tare (int): The weight of an empty carriage.
+            length (float): The length of a carriage.
+            volume (float, optional): The volume of a carriage. Defaults to 0.
+        """
         self.tare = tare
         self.length = length
         self.volume = volume
 
-    def set_volume(self, inputs):
-        if inputs > 0:
-            self.volume = inputs
+    def set_volume(self, volume):
+        """
+        Set the volume of a carriage.
+
+        Args:
+            volume (float): The volume of a carriage.
+        """
+        if volume > 0:
+            self.volume = volume
         else:
-            print("\nWrong value! \n Using default volume value of 0!")
+            raise ValueError("\nWrong value! Using default volume value of 0!")
