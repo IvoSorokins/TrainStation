8f427afc2aaff6036de9c08fecd6248ccfd15d4f
Ivo Sorokins <91875727+IvoSorokins@users.noreply.github.com>
2023-04-12 17:25:39 +0300

    Delete pythonIntermediate_I.Sorokins-master directory

--- pythonIntermediate_I.Sorokins-master/Livestock.py
+++
@@ -1,9 +0,0 @@
-from enum import Enum
-
-
-class Livestock(Enum):
-    CATTLE = "Cattle"
-
-    @classmethod  # Class method that returns a list of all the values in the enumeration class
-    def as_list(cls):  # cls refers to the enumeration class itself
-        return [cls.CATTLE.value]
