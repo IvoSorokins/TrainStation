021b48b1b2167e482c862a63f7dafaaac11d1041
Ivo Sorokins <91875727+IvoSorokins@users.noreply.github.com>
2023-04-13 10:13:33 +0300

    Added comments

--- Solids.py
+++ Solids.py
@@ -2,6 +2,17 @@ from enum import Enum
 
 
 class Solids(Enum):
+    """
+    An enumeration class that defines types of Solids.
+
+    Attributes:
+    - PAPER (str): the type of paper that can be carried
+    - FOOD (str): the type of food that can be carried
+    - METAL_ORE (str): the type of metal ore that can be carried
+    - WOOD (str): the type of wood that can be carried
+    - METAL_PIPES (str): the type of metal pipes that can be carried
+    - SHIPPING_CONTAINER (str): the type of shipping container that can be carried
+    """
     PAPER = "Paper"
     FOOD = "Food"
     METAL_ORE = "Metal ore"
@@ -9,6 +20,12 @@ class Solids(Enum):
     METAL_PIPES = "Metal pipes"
     SHIPPING_CONTAINER = "Shipping container"
 
-    @classmethod  # Class method that returns a list of all the values in the enumeration class
-    def as_list(cls):  # cls refers to the enumeration class itself
-        return [c.value for c in cls]  # iterates over  all the instances of enum class
+    @classmethod
+    def as_list(cls):
+        """
+        Returns a list of all the values in the Solid enumeration class.
+
+        Returns:
+        - list: a list of all the values in the enumeration class
+        """
+        return [c.value for c in cls]
