f29b4c087aeffa903960b636fe99d4b27a510c06
Ivo Sorokins <91875727+IvoSorokins@users.noreply.github.com>
2023-04-13 08:23:51 +0300

    Cleaned up , added comments

--- Gondola.py
+++ Gondola.py
@@ -8,25 +8,50 @@ class Gondola(Carriage):
     gd_counter = 0
 
     def __init__(self, tare, length, volume=0, cargo_type=Granular.COAL.value):
+        """
+            Initializes a Gondola object.
+
+            Args:
+            - tare (int): the weight of the empty carriage in kg
+            - length (int): the length of the carriage in meters
+            - volume (int): the volume of the carriage in cubic meters
+            - cargo_type (str): the type of cargo the cistern is carrying (default: Liquid.OIL.value)
+            """
         super().__init__(tare, length, volume)
         self.id = f"GD-{Gondola.gd_counter}"
         Gondola.gd_counter += 1
         self.cargo_type = cargo_type
-        self.volume = volume
 
-    def set_cargo_type(self, cargo_type):
-        if self.cargo_type in Gondola.valid_cargo_types:
-            self.cargo_type = cargo_type
+    @classmethod
+    def set_cargo_type(cls, cargo_type):
+        if cls.cargo_type in cls.valid_cargo_types:
+            cls.cargo_type = cargo_type
         else:
-            print("\nInvalid cargo type! \nUsing default cargo type - Coal!")
+            raise ValueError("Invalid cargo type! Valid types are: " + ", ".join(str(cls.valid_cargo_types)))
 
     def add_cargo(self, cargo_type, volume):
+        """
+                Adds cargo to the gondola.
+
+                Args:
+                - cargo_type (str): the type of cargo
+                - volume (int): the volume of the cargo in cubic meters
+
+                Raises:
+                - ValueError: if the cargo_type is not a valid type
+                """
         if cargo_type in Gondola.valid_cargo_types:
             self.cargo_type = cargo_type
             self.volume = volume
         else:
-            print("\nInvalid cargo type! \nUsing default cargo type - Grain!")
+            raise ValueError("Invalid cargo type! Valid types are: " + ", ".join(str(self.valid_cargo_types)))
 
         # Method that returns string that represents object
     def __repr__(self):
+        """
+                Returns a string representation of the cistern.
+
+                Returns:
+                - str: a string representation of the cistern
+                """
         return f"[Gondola] ID:{self.id} Cargo type: {self.cargo_type} Volume:{self.volume}"
