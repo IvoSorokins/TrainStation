935faf031c1a6363f72f07162727647e87647be8
Ivo Sorokins <91875727+IvoSorokins@users.noreply.github.com>
2023-04-13 10:15:21 +0300

    Added comments

--- Liquid.py
+++ Liquid.py
@@ -2,10 +2,25 @@ from enum import Enum
 
 
 class Liquid(Enum):
+    """
+    An enumeration class that defines types of liquids that can be carried by a tank.
+
+    Attributes:
+    - OIL (str): the type of oil that can be carried
+    - MILK (str): the type of milk that can be carried
+    - WATER (str): the type of water that can be carried
+    """
+
     OIL = "Oil"
     MILK = "Milk"
     WATER = "Water"
 
-    @classmethod  # Class method that returns a list of all the values in the enumeration class
-    def as_list(cls):  # cls refers to the enumeration class itself
-        return [c.value for c in cls]  # iterates over  all the instances of enum class
+    @classmethod
+    def as_list(cls):
+        """
+        Returns a list of all the values in the enumeration class.
+
+        Returns:
+        - List[str]: a list of all the values in the enumeration class
+        """
+        return [c.value for c in cls]
