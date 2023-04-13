fa630bbc1fb47dab52b7185641927cb4a2e307aa
Ivo Sorokins <91875727+IvoSorokins@users.noreply.github.com>
2023-04-13 10:14:54 +0300

    Added comments

--- Livestock.py
+++ Livestock.py
@@ -2,8 +2,20 @@ from enum import Enum
 
 
 class Livestock(Enum):
+    """
+        An enumeration class that defines types of livestock.
+
+        Attributes:
+        - CATTLE (str): the type of cattle that can be carried
+        """
     CATTLE = "Cattle"
 
-    @classmethod  # Class method that returns a list of all the values in the enumeration class
-    def as_list(cls):  # cls refers to the enumeration class itself
+    @classmethod
+    def as_list(cls):
+        """
+        Returns a list of all the values in the Livestock enumeration class.
+
+        Returns:
+        - list: a list of all the values in the enumeration class
+        """
         return [cls.CATTLE.value]
